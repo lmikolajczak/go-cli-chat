b46561803e31c77e0bffa3be62b88cbb082692e1
Luqqk <mikolajczak.luq@gmail.com>
2021-10-03 18:46:55 +0200

    Initial client UI implementation

---
+++ cmd/client/ui.go
@@ -0,0 +1,52 @@
+package main
+
+import "github.com/jroimartin/gocui"
+
+func layout(g *gocui.Gui) error {
+	maxX, maxY := g.Size()
+	g.Cursor = true
+
+	if messages, err := g.SetView("messages", 0, 0, maxX-20, maxY-5); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		messages.Title = " messages: "
+		messages.Autoscroll = true
+		messages.Wrap = true
+	}
+
+	if input, err := g.SetView("input", 0, maxY-5, maxX-20, maxY-1); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		input.Title = " send: "
+		input.Autoscroll = false
+		input.Wrap = true
+		input.Editable = true
+	}
+
+	if users, err := g.SetView("users", maxX-20, 0, maxX-1, maxY-1); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		users.Title = " users: "
+		users.Autoscroll = false
+		users.Wrap = true
+	}
+
+	if name, err := g.SetView("name", maxX/2-10, maxY/2-1, maxX/2+10, maxY/2+1); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		g.SetCurrentView("name")
+		name.Title = " name: "
+		name.Autoscroll = false
+		name.Wrap = true
+		name.Editable = true
+	}
+	return nil
+}
+
+func quit(g *gocui.Gui, v *gocui.View) error {
+	return gocui.ErrQuit
+}
