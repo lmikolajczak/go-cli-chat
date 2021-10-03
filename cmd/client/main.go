a2f39c35368514e3d4e46da6d78ff9ecadef2f71
Luqqk <mikolajczak.luq@gmail.com>
2021-10-03 18:46:55 +0200

    Initial client UI implementation

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -2,35 +2,41 @@ package main
 
 import (
 	"log"
-	"time"
 
-	"github.com/Luqqk/go-cli-chat/internal/data"
-	"golang.org/x/net/websocket"
+	"github.com/jroimartin/gocui"
 )
 
 func main() {
-	connection, err := websocket.Dial("ws://server:5000/", "", "http://server/")
+	g, err := gocui.NewGui(gocui.OutputNormal)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer connection.Close()
-	go func() {
-		for {
-			message := data.NewMessage()
-			err := websocket.JSON.Receive(connection, message)
-			if err != nil {
-				return
-			}
-			log.Println("message:", message)
-		}
-	}()
-	<-time.After(2 * time.Second)
-	message := data.NewMessage()
-	message.From = "Client"
-	message.Text = "test message from the client"
-	websocket.JSON.Send(connection, message)
-	<-time.After(2 * time.Second)
-	message.Text = "another message from the client"
-	websocket.JSON.Send(connection, message)
-	<-time.After(10 * time.Second)
+	defer g.Close()
+	g.SetManagerFunc(layout)
+	g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit)
+	g.MainLoop()
+	// connection, err := websocket.Dial("ws://server:5000/", "", "http://server/")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer connection.Close()
+	// go func() {
+	// 	for {
+	// 		message := data.NewMessage()
+	// 		err := websocket.JSON.Receive(connection, message)
+	// 		if err != nil {
+	// 			return
+	// 		}
+	// 		log.Println("message:", message)
+	// 	}
+	// }()
+	// <-time.After(2 * time.Second)
+	// message := data.NewMessage()
+	// message.From = "Client"
+	// message.Text = "test message from the client"
+	// websocket.JSON.Send(connection, message)
+	// <-time.After(2 * time.Second)
+	// message.Text = "another message from the client"
+	// websocket.JSON.Send(connection, message)
+	// <-time.After(10 * time.Second)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module github.com/Luqqk/go-cli-chat
 
 go 1.17
 
-require golang.org/x/net v0.0.0-20210917221730-978cfadd31cf
+require (
+	github.com/jroimartin/gocui v0.5.0
+	golang.org/x/net v0.0.0-20210917221730-978cfadd31cf
+)
+
+require (
+	github.com/mattn/go-runewidth v0.0.9 // indirect
+	github.com/nsf/termbox-go v1.1.1 // indirect
+)
