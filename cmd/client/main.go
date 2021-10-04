c43f04413597d016bd8512d24cf75d87ebb35a96
Luqqk <mikolajczak.luq@gmail.com>
2021-10-04 19:28:47 +0200

    Add connect UI func

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -13,23 +13,9 @@ func main() {
 	}
 	defer g.Close()
 	g.SetManagerFunc(layout)
+	g.SetKeybinding("name", gocui.KeyEnter, gocui.ModNone, connect)
 	g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit)
 	g.MainLoop()
-	// connection, err := websocket.Dial("ws://server:5000/", "", "http://server/")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// defer connection.Close()
-	// go func() {
-	// 	for {
-	// 		message := data.NewMessage()
-	// 		err := websocket.JSON.Receive(connection, message)
-	// 		if err != nil {
-	// 			return
-	// 		}
-	// 		log.Println("message:", message)
-	// 	}
-	// }()
 	// <-time.After(2 * time.Second)
 	// message := data.NewMessage()
 	// message.From = "Client"
